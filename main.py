6ad82a81238e20f03cf217edfdca843129464037
joaquin castor <castor.jm@obmontessori.edu.ph>
2026-09-15 19:02:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from pyscript import document, display
 
 def calculate_total(e):
     sub = (
-        float(document.getElementById('Burger').value) * document.getElementById('Burger').checked +
-        float(document.getElementById('Fries').value) * document.getElementById('Fries').checked +
-        float(document.getElementById('Drink').value) * document.getElementById('Drink').checked +
+        float(document.getElementById('Burger').value) * document.getElementById('Burger').checked
+        float(document.getElementById('Fries').value) * document.getElementById('Fries').checked
+        float(document.getElementById('Drink').value) * document.getElementById('Drink').checked
         float(document.getElementById('Dessert').value) * document.getElementById('Dessert').checked
     )
     
