5162a7dcf6cedd808db883317836ad61ea563a9c
joaquin castor <castor.jm@obmontessori.edu.ph>
2026-09-19 10:29:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from pyscript import document, display
 def calculate_total(e):
     document.getElementById("receipt").innerHTML = ""
 
-    item1 = document.getElementById('Burger')
+    item1 = document.getElementById('ting1')
     price1 = float(item1.value) * item1.checked
 
-    item2 = document.getElementById('Fries')
+    item2 = document.getElementById('ting2')
     price2 = float(item2.value) * item2.checked
 
-    item3 = document.getElementById('Drink')
+    item3 = document.getElementById('ting3')
     price3 = float(item3.value) * item3.checked
 
-    item4 = document.getElementById('Dessert')
+    item4 = document.getElementById('ting4')
     price4 = float(item4.value) * item4.checked
 
     subtotal = price1 + price2 + price3 + price4
